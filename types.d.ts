77065a7eba72effeb0238452d15df6816bdb0034
Vedant <122482472+vedantparmar12@users.noreply.github.com>
2025-10-05 12:55:06 +0530

    updating some new fast features

--- types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'robots-parser' {
-  interface RobotsParser {
-    isAllowed(url: string, userAgent?: string): boolean;
-  }
-
-  function robotsParser(url: string, content: string): RobotsParser;
-  
-  export default robotsParser;
-}
